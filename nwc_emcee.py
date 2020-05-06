64423f68695f095d51759b351a7bfcfa2df11e1f
Nelson Chen <crazysim@gmail.com>
2020-05-05 22:45:59 -0700

    initially test. not working!

---
+++ nwc_emcee.py
@@ -0,0 +1,35 @@
+import time
+
+import numpy as np
+import cv2
+
+from matcher import TitleMatcher
+
+cap = cv2.VideoCapture('D:\\Nintendo World Championships 1990 (U) [!].avi')
+# cap = cv2.VideoCapture(0)
+
+frame_number = 0
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    if TitleMatcher.match(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)):
+        title_match = "TITLE!"
+    else:
+        title_match = "NOT TITLE!"
+
+    # Display the resulting frame
+    cv2.putText(frame, title_match, (50,50), cv2.FONT_HERSHEY_SIMPLEX,
+                1, (255,0,255), 2, cv2.LINE_AA)
+    cv2.imshow('frame', frame)
+    frame_number += 1
+
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        status = cv2.imwrite(f'samples/snap_{frame_number}.png', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
