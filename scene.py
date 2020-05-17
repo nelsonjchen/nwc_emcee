a84786260731da8508691728ba6c62999979d7e2
Nelson Chen <crazysim@gmail.com>
2020-05-17 01:20:01 -0700

    rough difference

---
+++ scene.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+from numpy.core.multiarray import ndarray
+
+# TODO: Figure out API
+class TitleScreen:
+    def __init__(self):
+        raw_mask = cv2.imread('masks/title_blue.png')
+        self.mask = cv2.threshold(raw_mask, 10, 255, cv2.THRESH_BINARY)[1]
+
+    def match(self, image_hsv: ndarray) -> bool:
+        lower_bound = np.array([100, 50, 50])
+        upper_bound = np.array([120, 255, 255])
+
+        blue_mask = cv2.inRange(image_hsv, lower_bound, upper_bound)
+        xor_mask = cv2.bitwise_xor(self.mask, blue_mask)
+
+        return np.count_nonzero(xor_mask) < 500
+
